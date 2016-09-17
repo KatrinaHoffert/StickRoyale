9e2f1d32004302b6d93ca5222c61aedc2bf5ebe8
Kat Hoffert <kat.hoffert@gmail.com>
2016-09-16 23:58:50 -0600

    Created Unity project with menu
    Menu has keyboard navigation. Axes have been configured as per
    design document. Mouse disabled. Button handlers implemented. Project
    uses text assets and has metadata forced to files.
    
    Expanded .gitignore to account for the built player.

---
+++ Assets/Menus/MainMenuScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Keep track of what the currently selected object was so that if the mouse deselects it
+    // (a behavior that annoyingly cannot be disabled!), we immediately reselect the last selected
+    // button.
+    private GameObject lastSelectedButton;
+
+    void Start()
+    {
+        // Initially select the new game button
+        var defaultButtonSelection = GameObject.Find("NewGameButton");
+        EventSystem.current.SetSelectedGameObject(defaultButtonSelection);
+        lastSelectedButton = defaultButtonSelection;
+
+        // Not using the mouse -- hide it. In the editor, press escape to unlock the mouse (necessary
+        // so that you can stop playing the game.
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Check if the mouse has deselected the selection
+        if(EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelectedButton);
+        }
+        // Otherwise update the selection in case it changed
+        else
+        {
+            lastSelectedButton = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+
+    /// <summary>
+    /// Called when the "new game" button is pressed.
+    /// </summary>
+	public void NewGameButtonDown()
+    {
+        Debug.Log("New Game");
+    }
+
+    /// <summary>
+    /// Called when the "join game" button is pressed.
+    /// </summary>
+    public void JoinGameButtonDown()
+    {
+        Debug.Log("Join Game");
+    }
+
+    /// <summary>
+    /// Called when the "quit" button is pressed. Note: doesn't work in the editor.
+    /// </summary>
+    public void QuitButtonDown()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
