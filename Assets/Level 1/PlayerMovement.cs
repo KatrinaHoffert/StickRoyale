fb269cde78d502f8b012810bd6ce7bede1ca9e51
Erik Lindsay (egl602) <egl602@mail.usask.ca>
2016-09-27 14:19:11 -0600

    Movement
    Beginning of movement branch. Basic level and a couple of scripts. Also
    a block asset

---
+++ Assets/Level 1/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerMovement : NetworkBehaviour {
+    Vector2 playerMoveSpeed;
+    Vector2 playerStop;
+
+	// Use this for initialization
+	void Start () {
+        playerMoveSpeed = new Vector2(5f, 0f);
+        playerStop = playerMoveSpeed - playerMoveSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+	    if(Input.GetKeyDown("d")) {
+            GetComponent<MovementScript>().SendMessage("ChangeMoveForce", playerMoveSpeed);
+        }
+        if(Input.GetKeyUp("d"))
+        {
+            GetComponent<MovementScript>().SendMessage("ChangeMoveForce", playerStop);
+        }
+	}
+}
---
+++ Assets/MovementScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+    private Vector2 moveForce;
+    private Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+        moveForce = new Vector2(0f, 0f);
+        rigid = GameObject.FindObjectOfType<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        ApplyMoveForce(moveForce);
+    }
+
+    /// <summary>
+    /// Applies the moveforce to the gameObject
+    /// </summary>
+
+    private void ApplyMoveForce(Vector2 force)
+    {
+        rigid.AddForce(force);
+    }
+    /// <summary>
+    /// Changes the force of the movement
+    /// </summary>
+    /// <param name="newForce"></param>
+    void ChangeMoveForce(Vector2 newForce)
+    {
+        moveForce = newForce;
+    }
+}
