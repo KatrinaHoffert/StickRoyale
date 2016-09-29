93f7422bb5caef8c1bb07120b3c01385e901c0a0
Kat Hoffert <kat.hoffert@gmail.com>
2016-09-29 12:40:31 -0600

    Naming consistency and some bug fixes

--- Assets/Menus/CharacterSelectMenuScript.cs
+++ Assets/Menus/CharacterSelectMenuScript.cs
@@ -60,7 +60,7 @@ public class CharacterSelectMenuScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        ClearDisconnectedPlayers();
+        if(!Global.PlayerIsClient()) ClearDisconnectedPlayers();
     }
 
     /// <summary>
