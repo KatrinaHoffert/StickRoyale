ee0f7c67bb1c6f08eba6847f00c278aee3ffc8a8
Kat Hoffert <kat.hoffert@gmail.com>
2016-09-29 12:33:40 -0600

    Added a "disconnected" screen

---
+++ Assets/Menus/DisconnectedScreenScript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DisconnectedScreenScript : MonoBehaviour
+{
+    public void ContinueButtonClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
