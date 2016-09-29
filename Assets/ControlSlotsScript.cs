0d049e0427414d07814e787f631b2775e1e1c197
Kat Hoffert <kat.hoffert@gmail.com>
2016-09-28 21:14:50 -0600

    Control slots appear in unity editor now

--- Assets/ControlSlotsScript.cs
+++ Assets/ControlSlotsScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using System;
 
 public class ControlSlotsScript : NetworkBehaviour
 {
@@ -47,6 +48,7 @@ public enum ControlType
 /// <summary>
 /// A player slot.
 /// </summary>
+[Serializable]
 public class ControlSlot
 {
     /// <summary>
