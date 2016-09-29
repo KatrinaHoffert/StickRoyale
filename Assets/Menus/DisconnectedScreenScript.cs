a13227e06d25234dca4fc5cc7cb80defe667ab54
Kat Hoffert <kat.hoffert@gmail.com>
2016-09-29 12:44:40 -0600

    Disconnect screen does cleanup

--- Assets/Menus/DisconnectedScreenScript.cs
+++ Assets/Menus/DisconnectedScreenScript.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class DisconnectedScreenScript : MonoBehaviour
 {
+    void Start()
+    {
+        while (GameObject.Find("ControlSlots") != null) DestroyImmediate(GameObject.Find("ControlSlots"));
+        while (GameObject.Find("NetworkManager") != null) DestroyImmediate(GameObject.Find("NetworkManager"));
+    }
+
     public void ContinueButtonClicked()
     {
         SceneManager.LoadScene("MainMenu");
