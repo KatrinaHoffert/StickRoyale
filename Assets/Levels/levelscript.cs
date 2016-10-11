8f4503eab81e6ac1dec4f99efbd84089b56df8cd
jox561 <jox561@mail.usask.ca>
2016-10-11 08:49:56 -0600

    level selection
    The level select menu has been added
    When a level is selected the players are spawned into the spawnpoints
    A added some basic movement to the players, this works in multiplayer.
    Each player is controlled by one client.
    I didn't commit this to the main branch because the character selection
    menu spawns players for some reason.

---
+++ Assets/Levels/levelscript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class levelscript : MonoBehaviour {
+
+    private GameObject [] players = new GameObject[4];
+    private NetworkStartPosition[] spawnPoints;
+    private int[] hitpoints = new int[4];
+   
+
+    // Use this for initialization
+    void Start () {
+        //CharacterSelectMenuScript.controlSlots[0].networkPlayerId
+        spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        players = GameObject.FindGameObjectsWithTag("Player");
+        for (int i=0; i<players.Length; i++)
+        {
+
+            if (CharacterSelectMenuScript.controlSlots[i].controlType != 0)
+            {
+
+               //puts players in spawn points
+                players[i].transform.position = spawnPoints[i].transform.position;
+                hitpoints[i] = 100;
+                //status[i] = new GameObject();
+                //status[i].AddComponent<Text>();
+                //status[i].transform.position = new Vector2(i * 200 - 400, 400);
+
+                //status[i] = 
+                //Text status = GameObject.Find("Text").GetComponent<Text>();
+                 //status.text = players[i].GetComponent<PlayerBase>().playerName + "\n" + players[i].GetComponent<PlayerBase>().uniquePlayerId + "\n" + hitpoints[i] + "/100";
+                
+                //GameObject.Find("Canvas").
+
+            }
+                        
+        }
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
