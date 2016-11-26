91a4fa0a1bf98b769a7ca2387d7332e9f91fff4b
Kat Hoffert <kat.hoffert@gmail.com>
2016-11-25 19:15:05 -0600

    Better way to handle the trigger

--- Assets/CharacterScripts/MageAttack1Trigger.cs
+++ Assets/CharacterScripts/MageAttack1Trigger.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MageAttack1Trigger : MonoBehaviour
 {
+    List<GameObject> playersAlreadyHit = new List<GameObject>();
+
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.CompareTag("Player"))
+        if (coll.gameObject.CompareTag("Player") && !playersAlreadyHit.Contains(coll.gameObject))
         {
             coll.gameObject.GetComponent<CharacterBase>().Damage(15);
             coll.gameObject.GetComponent<CharacterBase>().DamageForce(gameObject.GetComponentInParent<Transform>().forward * 100);
+            playersAlreadyHit.Add(coll.gameObject);
         }
-
-        GameObject.Destroy(gameObject);
     }
 }
