fc68c4303ada2dbacafdc0229ea6a65b74546f7e
Kat Hoffert <kat.hoffert@gmail.com>
2016-12-06 19:17:25 -0600

    Invincible powerup makes you blink

--- Assets/Powerups/PowerupInvincible.cs
+++ Assets/Powerups/PowerupInvincible.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PowerupInvincible : PowerupBase
 {
+    /// <summary>
+    /// Period of blinking phases.
+    /// </summary>
+    public float blinkPeriod = 0.2f;
+
+    public float animationStartTime;
+
     public override float GetDuration()
     {
         return 5f;
@@ -10,14 +18,19 @@ public class PowerupInvincible : PowerupBase
 
     public override float GetAiWeight(CharacterBase character)
     {
-        
         return 2.0f;
     }
 
     public override void ApplyStart(CharacterBase character)
     {
         character.invincible = true;
-        character.GetComponent<SpriteRenderer>().color = Color.yellow;
+        animationStartTime = Time.time;
+    }
+
+    public override void ApplyUpdate(CharacterBase character)
+    {
+        var elapsedTime = Time.time - animationStartTime;
+        character.gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, (elapsedTime % blinkPeriod) / blinkPeriod);
     }
 
     public override void ApplyEnd(CharacterBase character)
